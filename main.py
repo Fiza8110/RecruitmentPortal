6af3e2be310766e2e55b684516dbfb1a4b21bfa9
Fiza8110 <fizajfizacg8@gmail.com>
2025-05-23 20:29:22 +0530

    Added port

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from routes.login import route as login #router object from routes/login.py
 from routes.home import route as home #router object from routes/home.py
 from routes.resetpassword import reset_router# instance for fastapi 
 from starlette.middleware.sessions import SessionMiddleware
+import uvicorn
+import os
 app = FastAPI() # app is the main app object you'll use to define routes, settings, middlewares, etc.
 
 # To use the html folder
@@ -14,7 +16,8 @@ templates = Jinja2Templates(directory='templates') # is used to render HTML temp
  # directory='templates': This tells FastAPI that your HTML files are stored in a folder named templates.
 # Mount the static folder for css, js, images
 app.mount("/static", StaticFiles(directory = "static"), name = "static")
-
+port = int(os.environ.get("PORT", 8000))  # Use 8000 locally, use PORT on Render
+uvicorn.run("main:app", host="0.0.0.0", port=port)
 # Include the routes 
 # Add this middleware (choose your own secret key)
 app.add_middleware(SessionMiddleware, secret_key="your-random-secret")
